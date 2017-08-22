18b261c5f7abcc222c8a4237a6e960a9f33063d4
Fan Ouyang <ouyan064@umn.edu>
2017-08-22 16:16:14 -0500

    add files

---
+++ reg_model_r_code copy.R
@@ -0,0 +1,131 @@
+install.packages("UsingR"); library(UsingR); data(galton); library(reshape); 
+long <- melt(galton)
+g <- ggplot(long, aes(x = value, fill = variable))
+g <- g + geom_histogram(colour = "black", binwidth=1)
+g <- g + facet_grid(. ~ variable)
+g
+
+
+library(manipulate)
+myHist <- function(mu){
+  mse <- mean((galton$child - mu)^2)
+  g <- ggplot(galton, aes(x = child)) + geom_histogram(fill = "salmon", 
+                                                       colour= "black", binwidth=1)
+  g <- g + geom_vline(xintercept = mu, size = 3)
+  g <- g + ggtitle(paste("mu = ", mu, ", MSE = ", round(mse, 2), sep = ""))
+  g
+}
+manipulate(myHist(mu), mu = slider(62, 74, step = 0.5))
+
+
+g <- ggplot(galton, aes(x = child)) + 
+  geom_histogram(fill = "salmon", colour = "black", binwidth=1)
+g <- g + geom_vline(xintercept = mean(galton$child), size = 3)
+g
+
+
+y <- galton$child - mean(galton$child)
+x <- galton$parent - mean(galton$parent)
+freqData <- as.data.frame(table(x, y))
+names(freqData) <- c("child", "parent", "freq")
+freqData$child <- as.numeric(as.character(freqData$child))
+freqData$parent <- as.numeric(as.character(freqData$parent))
+myPlot <- function(beta){
+  g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+  g <- g  + scale_size(range = c(2, 20), guide = "none" )
+  g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+  g <- g + geom_point(aes(colour=freq, size = freq))
+  g <- g + scale_colour_gradient(low = "lightblue", high="white")
+  g <- g + geom_abline(intercept = 0, slope = beta, size = 3)
+  mse <- mean( (y - beta * x) ^2 )
+  g <- g + ggtitle(paste("beta = ", beta, "mse = ", round(mse, 3)))
+  g
+}
+manipulate(myPlot(beta), beta = slider(0.6, 1.2, step = 0.02))
+
+
+lm(I(child - mean(child))~ I(parent - mean(parent)) - 1, data = galton)
+
+
+y <- galton$child
+x <- galton$parent
+beta1 <- cor(y, x) *  sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+rbind(c(beta0, beta1), coef(lm(y ~ x)))
+
+beta1 <- cor(y, x) *  sd(x) / sd(y)
+beta0 <- mean(x) - beta1 * mean(y)
+rbind(c(beta0, beta1), coef(lm(x ~ y)))
+
+yc <- y - mean(y)
+xc <- x - mean(x)
+beta1 <- sum(yc * xc) / sum(xc ^ 2)
+c(beta1, coef(lm(y ~ x))[2])
+
+yn <- (y - mean(y))/sd(y)
+xn <- (x - mean(x))/sd(x)
+c(cor(y, x), cor(yn, xn), coef(lm(yn ~ xn))[2])
+
+
+library(UsingR)
+data(father.son)
+y <- (father.son$sheight - mean(father.son$sheight)) / sd(father.son$sheight)
+x <- (father.son$fheight - mean(father.son$fheight)) / sd(father.son$fheight)
+rho <- cor(x, y)
+library(ggplot2)
+g = ggplot(data.frame(x, y), aes(x = x, y = y))
+g = g + geom_point(size = 5, alpha = .2, colour = "black")
+g = g + geom_point(size = 4, alpha = .2, colour = "red")
+g = g + geom_vline(xintercept = 0)
+g = g + geom_hline(yintercept = 0)
+g = g + geom_abline(position = "identity")
+g = g + geom_abline(intercept = 0, slope = rho, size = 2)
+g = g + geom_abline(intercept = 0, slope = 1 / rho, size = 2)
+g = g + xlab("Father's height, normalized")
+g = g + ylab("Son's height, normalized")
+g
+
+
+library(UsingR)
+data(diamond)
+library(ggplot2)
+g = ggplot(diamond, aes(x = carat, y = price))
+g = g + xlab("Mass (carats)")
+g = g + ylab("Price (SIN $)")
+g = g + geom_point(size = 7, colour = "black", alpha=0.5)
+g = g + geom_point(size = 5, colour = "blue", alpha=0.2)
+g = g + geom_smooth(method = "lm", colour = "black")
+g
+
+fit <- lm(price ~ carat, data = diamond)
+coef(fit)
+fit2 <- lm(price ~ I(carat - mean(carat)), data = diamond)
+coef(fit2)
+fit3 <- lm(price ~ I(carat * 10), data = diamond)
+coef(fit3)
+newx <- c(0.16, 0.27, 0.34)
+coef(fit)[1] + coef(fit)[2] * newx
+predict(fit, newdata = data.frame(carat = newx))
+
+
+data(diamond)
+y <- diamond$price; x <- diamond$carat; n <- length(y)
+fit <- lm(y ~ x)
+## The easiest way to get the residuals
+e <- resid(fit)
+## Obtain the residuals manually, get the predicted Ys first
+yhat <- predict(fit)
+## The residuals are y - yhat. Let's check by comparing this
+## with R's build in resid function
+max(abs(e -(y - yhat)))
+## Let's do it again hard coding the calculation of Yhat
+max(abs(e - (y - coef(fit)[1] - coef(fit)[2] * x)))
+
+
+library(UsingR); data(diamond)
+y <- diamond$price; x <- diamond$carat; n <- length(y)
+beta1 <- cor(y, x) * sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+e <- y - beta0 - beta1 * x
+sigma <- sqrt(sum(e^2) / (n-2))
+ssx <- sum((x - mean(x))^2)
